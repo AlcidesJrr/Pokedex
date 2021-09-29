97d3b2f628d84cde9e317d5b2271eb9cf4272d2a
Alcides dos Santos Junior <alcides.jrr@gmail.com>
2021-09-29 15:26:54 -0300

    Primeira Versão

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+const bodyParser = require("body-parser");
+
+let message = "";
+
+const pokedex = [];
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+
+    setTimeout(() => {
+        message = "";
+      }, 1000);
+
+    res.render("index", {
+        pokedex: pokedex,
+        message,
+    });
+});
+
+app.get("/cadastro", (req, res) => {
+    res.render("cadastro");
+});
+  
+app.get("/detalhes/:id", (req, res) => {
+    const id = req.params.id;
+    const pokemon = pokedex[id];
+    res.render("detalhes", {
+        pokemon,
+    });
+});
+
+app.post("/new", (req, res) => {
+    const pokemon = req.body;
+    pokedex.push(pokemon);
+    message = "Pokemon criado com sucesso!";
+    res.redirect("/");
+  });
+
+  app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
+
